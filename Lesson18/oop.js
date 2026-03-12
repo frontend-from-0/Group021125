28e5a83f4d64210598b4fdf21353e629e7945e8b
annasvst <annaatalay@hotmail.com>
2026-03-12 20:22:22 +0100

    Lesson 18 completed

--- Lesson18/oop.js
+++ Lesson18/oop.js
@@ -12,18 +12,55 @@
 
 // Real life situation: we have multiple users on our website, and we want to store their information in a way that is easy to access and modify. Different users have different roles, and we want to be able to easily change their roles. We also need to make sure that all users have the same properties.
 
-const user1 = {
-  username: 'John',
-  email: 'john@gmail.com',
-  role: 'user'
-};
+// const user1 = {
+//   username: 'John',
+//   email: 'john@gmail.com',
+//   role: 'user'
+// };
 
-const user2 = {
-  username: 'Jane',
-  email: 'jane@gmail.com'
+// const user2 = {
+//   username: 'Jane',
+//   email: 'jane@gmail.com'
+// }
+
+// const user3 = {
+//   name: 'Adam',
+//   email: 'adam@gmail.com'
+// }
+
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+    this.role = 'user';
+  }
+
+  describe () {
+    console.log(`This is the user with username ${this.username}, email ${this.email}, and role ${this.role}`)
+  }
 }
 
-const user3 = {
-  name: 'Adam',
-  email: 'adam@gmail.com'
+class AdminUser extends User {
+  #adminId;
+  constructor(username, email) {
+    super(username, email);
+    this.role = 'admin';
+    this.#adminId = Date.now();
+  }
+
+  loginAsAdmin() {
+    console.log('Logging in as admin user...');
+  }
 }
+
+
+const user1 = new User('John', 'john@gmail.com');
+const user2 = new User('Jane', 'jane@gmail.com');
+const user3 = new AdminUser('Adam', 'adam@gmail.com');
+
+console.log(user1, user2, user3);
+
+user1.describe();
+user2.describe();
+user3.describe();
+user3.loginAsAdmin();
--- Lesson18/script.js
+++ Lesson18/script.js
@@ -28,6 +28,125 @@ Node.js or a browser console.
 3. Add a `viewCart` method to display all items in the cart.
 */
 
+class ShoppingCart {
+  #items;
+  #discounts = {
+    SAVE10: 0.1,
+    SAVE20: 0.2,
+  };
+
+  constructor() {
+    this.#items = [];
+  }
+
+  viewCart() {
+    console.log('Viewing cart...');
+
+    if (this.#items.length > 0) {
+      for (const item of this.#items) {
+        console.log(
+          `Name: ${item.name}, Price: ${item.price.amount} ${item.price.currency}, Quantity: ${item.quantity}`,
+        );
+      }
+    } else {
+      console.log('Cart is empty.');
+    }
+
+    console.log('-------------------');
+  }
+
+  addItem(name, price, quantity) {
+    console.log('Adding item to the cart...');
+    for (const item of this.#items) {
+      if (item.name === name) {
+        console.log(
+          `The item with name ${name} already exists. Incrementing quantity by ${quantity}`,
+        );
+        item.quantity += quantity;
+        console.log('-------------------');
+        return;
+      }
+    }
+    console.log(
+      `Item with name ${name} is not found, adding a new item to the cart.`,
+    );
+    this.#items.push({ name, price, quantity }); // {name: name, price:price, quantity: quantity}
+
+    console.log('-------------------');
+  }
+
+  removeItem(name) {
+    console.log('Removing item..');
+    for (let i = 0; i < this.#items.length; i++) {
+      if (this.#items[i].name === name) {
+        this.#items.splice(i, 1);
+        console.log(`Item with name ${name} was succsessfully removed!`);
+        return;
+      }
+    }
+    console.log(`Item with name ${name} doesn\'t exist.`);
+  }
+
+  getTotal() {
+    console.log('Calculating total.');
+    let total = 0;
+    for (let i = 0; i < this.#items.length; i++) {
+      total += this.#items[i].price.amount * this.#items[i].quantity;
+    }
+    console.log(
+      'The total amount is ',
+      total + ' ' + this.#items[0].price.currency,
+    );
+    return total;
+  }
+
+  applyDiscount(discountCode) {
+    const maybeDiscount = this.#discounts[discountCode];
+    if (maybeDiscount) {
+      const total = this.getTotal();
+      const totalAfterDiscount = total - total * maybeDiscount;
+      console.log(`Total after discount ${discountCode}: ${totalAfterDiscount}`);
+      return totalAfterDiscount;
+    } else {
+      console.log(`Discount code ${discountCode} is not valid`);
+      return null;
+    }
+  }
+}
+
+// This class helps us create a common structure for all items that we use
+class Item {
+  constructor(name, price) {
+    if (typeof name === 'string' && name.trim().length > 2) {
+      this.name = name;
+    } else {
+      throw Error('Name should be a string with more than 2 characters');
+    }
+
+    // TODO add validation to verify that price is an object with amount and currency keys, and that the currency is in uppercase 3 letter format
+    this.price = price;
+  }
+}
+
+const laptop = new Item('Laptop', { amount: 1000, currency: 'EUR' });
+const phone = new Item('Phone', { amount: 800, currency: 'EUR' });
+
+const shoppingCart1 = new ShoppingCart();
+shoppingCart1.viewCart();
+shoppingCart1.addItem(laptop.name, laptop.price, 1);
+shoppingCart1.addItem(laptop.name, laptop.price, 2);
+shoppingCart1.addItem(phone.name, phone.price, 5);
+shoppingCart1.viewCart();
+shoppingCart1.getTotal();
+
+shoppingCart1.removeItem(laptop.name);
+
+shoppingCart1.viewCart();
+shoppingCart1.getTotal();
+
+shoppingCart1.applyDiscount('SAVE30');
+shoppingCart1.applyDiscount('SAVE10');
+
 /*
 -----------------------------------------------------------
   STEP 2: Add Items to the Cart
@@ -71,5 +190,3 @@ Node.js or a browser console.
      code is valid.
 3. Use an object to store discount codes and their values.
 */
-
-
